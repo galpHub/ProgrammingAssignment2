68bf7b5f977ae7aaab110938f5c1b75e6a4c5d1d
galpHub <galpHub@users.noreply.github.com>
2015-02-08 14:02:30 -0700

    Added useful comments explaining the previous edits to the cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix is a function that creates other functions for
+## storing a matrix and computing its inverse (if not done previously).
+
+## cacheSolve is a function that will use the output of makeCacheMatrix
+## function in order to extract the precomputed value of the inverse 
+## matrix (if stored) and computing it otherwise.
+
 ## Write a short comment describing this function
+
+## Outputs the list of set/get function pairs for setting/getting a 
+## matrix-inverseMatrix pair
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -19,6 +29,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## This function uses the set/get functions outputted by the 
+## makeCacheMatrix function in order to extract the inverse matrix
+## that was previously stored in the enviroment of definition of
+## makeCacheMatrix and compute it on the fly otherwise.
 
 cacheSolve <- function(x) {
          m <- x$getInverse()
